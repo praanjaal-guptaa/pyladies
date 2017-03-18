cd0cea3150980dae869ce193f51b41a454b8afa0
Anna Makarudze <amakarudze@gmail.com>
2017-03-18 13:35:38 +0200

    Heroku settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn harare.wsgi --log-file -
--- harare/settings.py
+++ harare/settings.py
@@ -82,15 +82,21 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 WSGI_APPLICATION = 'harare.wsgi.application'
 
 MARKITUP_FILTER = ('markdown.markdown', {'safe_mode': True})
+
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}"""
+
+# Update database configuration with $DATABASE_URL
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # AUTH_USER_MODEL = 'auth.User'
 
@@ -128,7 +134,17 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

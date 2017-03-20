0511ef9cfd95400a325f10a19191e5b35297cb1c
Charle <katuricharles@gmail.com>
2017-03-20 15:28:49 +0200

    added runtime.txt and made changes to setting.py

--- harare/settings.py
+++ harare/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production

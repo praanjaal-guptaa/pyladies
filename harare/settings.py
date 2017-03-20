b58dff6e279657aedd9f069fca2ea451f1f23425
Charle <katuricharles@gmail.com>
2017-03-20 10:38:36 +0200

    Uncommented original database settings

--- harare/settings.py
+++ harare/settings.py
@@ -90,12 +90,12 @@ MARKITUP_FILTER = ('markdown.markdown', {'safe_mode': True})
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}"""
+}
 
 # Update database configuration with $DATABASE_URL
 db_from_env = dj_database_url.config(conn_max_age=500)
